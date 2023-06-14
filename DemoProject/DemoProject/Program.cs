3174d8e10b0e71307b909df3bd5b289d3c0e9687
flowerrz <120988660+flowerrz@users.noreply.github.com>
2023-06-14 16:15:59 +0300

    Added method of calculating the area of a square

--- DemoProject/DemoProject/Program.cs
+++ DemoProject/DemoProject/Program.cs
@@ -8,13 +8,22 @@ namespace DemoProject
         {
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
+
             Console.WriteLine("The area is:");
             Console.WriteLine(GetRegtangleArea(a, b));
+
+            Console.WriteLine("Area of a square with sides 2:");
+            Console.WriteLine(GetSquareArea(2));
         }
 
         public static double GetRegtangleArea(double a, double b)
         {
             return a * b;
+        }
+
+        public static double GetSquareArea(double a)
+        {
+            return a * a;
         }
     }
 }
